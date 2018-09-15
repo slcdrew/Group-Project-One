f88343db87014c9579061ec2422f71533b0fb84a
Andrew Pederson <apederson@inmoment.com>
2018-09-14 18:27:46 -0600

    adding pseudocode

---
+++ Assets/Javascript/app.js
@@ -0,0 +1,20 @@
+//on click upload image (nested functions)
+//image saves to cloudinary DB
+//image is scanned by Face++ returns tags in an object   need to bring in variable emotion attribute and sort by math.max() (see slack group)
+//Object tags are stored in an array?
+//API call to quote rest service (theysaidso.com/api) and pass face tags as paramters to return quote object
+//Does Face++ tagging and quote object tag write back to cloudinary???? R&D is needed here!!
+//Display image with  quote on DOM
+
+
+//**If image tag is neautral the quote service must be passed a value of 'indifferent' */
+
+//http://quotes.rest/#!/quote/get_quote_search
+//API KEY lUUqEhaz6FZnQ4lxr_WHoAeF
+
+//https://cloudinary.com/console
+//UN: andrew.mpederson@gmail.com
+//PW: Inmoment1!
+
+
+
